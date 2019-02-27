6351db2cab744b2b6e8d59d108041f232b0994aa
Thando <bukhosi95@gmail.com>
2019-02-27 20:21:15 +0000

    added thumbnails to use for user interface

--- app/src/main/java/com/google/ar/sceneform/Overlei/Overlei.java
+++ app/src/main/java/com/google/ar/sceneform/Overlei/Overlei.java
@@ -43,7 +43,7 @@ public class Overlei extends AppCompatActivity {
   private static final double MIN_OPENGL_VERSION = 3.0;
 
   private ArFragment arFragment;
-  private ModelRenderable andyRenderable;
+  //private ModelRenderable andyRenderable;
 
   @Override
   @SuppressWarnings({"AndroidApiChecker", "FutureReturnValueIgnored"})
